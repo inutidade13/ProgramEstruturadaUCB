b92be57b6b794923e1f742356bab526ebe76b535
Erik Daniel <erikdaniel.souza@gmail.com>
2023-03-21 21:03:05 -0300

    lista01exer2

---
+++ AulaProgramEtruturada/atividade_02.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+
+int main()
+{
+
+    int numeroInteiro;
+    float numeroDecimal1, numerodecimal2;
+    double numerodouble;
+    char umastring[100], umaLetra;
+    
+    
+
+
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numeroInteiro);
+
+    printf("Digite dois numeros reais: ");
+    scanf("%f%f", &numeroDecimal1, &numerodecimal2);
+
+    printf("Digite um numero real double: ");
+    scanf("%lf", &numerodouble);
+
+    printf("Digite uma letra: ");
+    scanf("%c", &umaLetra);
+
+    printf("Digite seu nome: ");
+    fgets(umastring, 100, stdin);
+
+
+    
+
+
+    
+
+
+
+
+
+}
