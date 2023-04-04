a8e2577f7a2a7ebff911fd3fcdeb40b92e0363cb
erik <erikdaniel.souza13@gmail.com>
2023-04-04 20:42:25 -0300

    Atividade da aula

---
+++ AulaProgramEtruturada/repeticao.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main ()
+{
+    
+
+    int i, repeticao1, repeticao2;
+
+    printf("Insira um numero:");
+    scanf("%d", &repeticao1);
+
+    printf("Insira um numero:");
+    scanf("%d", &repeticao2);
+
+
+
+    for ( i = repeticao1; i < repeticao2; i++)
+    {
+        if ( i % 2 == 1)
+        {
+            printf("%d\n", i);
+        }
+       
+    }
+    
+}
+
+
